d78b47e42b32b51fc94b46c7def675ced3448b72
EmmaNeble <100777956+EmmaNeble@users.noreply.github.com>
2022-04-01 11:38:54 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,34 @@
+##Getting data ready
+#Data download
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+#Data set to date
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#Filter data  from Feb. 1, 2007 until Feb. 2, 2007
+data <- subset(data,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+#Remove any incomplete observation
+data <- data[complete.cases(data),]
+
+#Combine the Date and Time column
+dateTime <- paste(data$Date, data$Time)
+
+#Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+#Remove Date and Time column
+data <- data[ ,!(names(data) %in% c("Date","Time"))]
+
+#Add DateTime column
+data <- cbind(dateTime, data)
+
+#Format dateTime Column
+data$dateTime <- as.POSIXct(dateTime)
+
+##Making the first plot
+hist(data$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
+#File is saved 
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
